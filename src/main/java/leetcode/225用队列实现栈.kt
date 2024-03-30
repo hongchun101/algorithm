02f712038446e7b8f6b5b102d6881fb3e7e44e4a
hongchun101 <1251341510@qq.com>
2024-03-30 23:12:51 +0800

    225

--- src/main/java/leetcode/225用队列实现栈.kt
+++ src/main/java/leetcode/225用队列实现栈.kt
@@ -1,24 +1,33 @@
 package leetcode
 
+import java.util.LinkedList
+import java.util.Queue
+
 class `225用队列实现栈` {
 }
 
 class MyStack() {
 
-    fun push(x: Int) {
+    var queue = LinkedList<Int>()
 
+    fun push(x: Int) {
+        queue.offer(x)
+        val size = queue.size
+        for (e in 0..<size - 1) {
+            queue.add(queue.poll())
+        }
     }
 
     fun pop(): Int {
-
+        return queue.poll()
     }
 
     fun top(): Int {
-
+        return queue.peek()
     }
 
     fun empty(): Boolean {
-
+        return queue.isEmpty()
     }
 
 }
