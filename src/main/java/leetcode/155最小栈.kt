524db6217c43b4a29c6b48e86162242cbca87d9f
hongchun101 <1251341510@qq.com>
2024-03-30 23:24:44 +0800

    155

---
+++ src/main/java/leetcode/155最小栈.kt
@@ -0,0 +1,36 @@
+package leetcode
+
+import java.util.Stack
+import kotlin.math.min
+
+class `155最小栈` {
+}
+
+class MinStack() {
+
+    var stack = Stack<Int>()
+    var minStack = Stack<Int>()
+
+    fun push(`val`: Int) {
+        stack.push(`val`)
+        if (minStack.isEmpty() || `val` <= minStack.peek()) {
+            minStack.push(`val`)
+        } else {
+            minStack.push(minStack.peek())
+        }
+    }
+
+    fun pop() {
+        stack.pop()
+        minStack.pop()
+    }
+
+    fun top(): Int {
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return minStack.peek()
+    }
+
+}
